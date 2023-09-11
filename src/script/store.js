5633852811bff6107c9bcabef12e8577422b83d4
Vladimir Belich <belichvladimir@mail.ru>
2023-09-11 18:51:13 +0300

    add templates, fix readme

---
+++ src/script/store.js
@@ -0,0 +1,8 @@
+
+export default window.application = {
+    blocks: {},
+    screens: {},
+    renderScreen: function(screenName) {},
+    renderBlock: function(blockName, container) {},
+    timers: []
+  }
